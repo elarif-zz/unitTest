553233263968ef8b9d73e20d257f4fd8716576e7
moustanir <moustanir.souleali@gmail.com>
2017-08-22 22:06:52 +0200

    case there no @

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -12,6 +12,27 @@ final class EmailTest extends TestCase
 {
 	public function testCanBeUsedAsString()
 	{
-		$this->assertEquals(true,Email::validate('Abc@example.com'));
+		$data = [    'prettyandsimple@example.com',
+			'very.common@example.com',
+			'disposable.style.email.with+symbol@example.com',
+			'other.email-with-dash@example.com',
+			'fully-qualified-domain@example.com.',
+			'x@example.com (one-letter local-part)',
+			'"very.unusual.@.unusual.com"@example.com',
+			'"very.(),:;<>[]\".VERY.\"very@\\ \"very\".unusual"@strange.example.com',
+			'example-indeed@strange-example.com',
+			'admin@mailserver1'];
+
+		foreach($data as $value)
+		{
+			$this->assertEquals(true,Email::validate($value));
+		}
+	}
+
+	public function shouldReturnFalseWhenThereNoAroBase()
+	{
+			
+		$value = 'Abc.example.com';
+		$this->assertEquals(false,Email::validate($value));
 	}
 }
