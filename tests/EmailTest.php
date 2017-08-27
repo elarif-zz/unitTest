c0eb5f3fdc289e1476c81011f86e42f00609bca4
moustanir <moustanir.souleali@gmail.com>
2017-08-27 18:21:57 +0200

    work in progress

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -10,38 +10,16 @@ use QCheck\Quick;
  */
 
 final class EmailTest extends TestCase{
-	public function testShouldReturnTrueWhenValidEmail(){
-		$validEmails = ['prettyandsimple@example.com',
-			'very.common@example.com',
-			'disposable.style.email.with+symbol@example.com',
-			'other.email-with-dash@example.com',
-			'fully-qualified-domain@example.com.',
-			'x@example.com (one-letter local-part)',
-			'"very.unusual.@.unusual.com"@example.com',
-			'"very.(),:;<>[]\".VERY.\"very@\\ \"very\".unusual"@strange.example.com',
-			'example-indeed@strange-example.com',
-			'admin@mailserver1'];
-
-		$mail = $this->createMock(Email::class);
-		$mail->method('hasArobase')->willReturn(true);
-		$mail->method('hasOnlyOneArobase')->willReturn(true);
-		foreach($validEmails as $email){
-			$this->assertEquals(true,$mail->validate($email));
-		}
-	}
-
 	public function testShouldReturnFalseWhenThereNoArobase(){
-		$stringsAreNeverNumeric = Gen::forAll(
+		$mail = new Email();
+		$stringsWithoutMail = Gen::forAll(
 			[Gen::strings()],
 			function($str) {
 				$mail->validate(str_replace("@",' ',$str));
 			});
-		$result = Quick::check(1000, $stringsAreNeverNumeric);
+		$result = Quick::check(1000, $stringsWithoutMail);
 		$this->assertFalse($result['result']);
+
 	}
 
-	public function testShouldReturnFalseWhenThereMoreThanOneArobase(){
-		$value = 'A@b@c@example.com';
-		$this->assertEquals(false,$mail->validate($value));
-	}
 }
